acba9ef13303089b964e3ea8fdfcaa00cd8482b9
Tanmay Mahendra Kothale <tanmaymkothale@gmail.com>
2022-04-26 01:48:34 +0100

    client code updated

---
+++ sample_socket/socket_client/lcd.c
@@ -0,0 +1,136 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "lcd.h"
+
+//Defines for GPIO pins on Raspberry Pi
+#define LCD_E 23
+#define LCD_RS 22
+#define LCD_D4 24
+#define LCD_D5 25
+#define LCD_D6 8
+#define LCD_D7 7
+
+void pulseEnable ()
+{
+   digitalWrite (LCD_E, HIGH) ; 
+   delay(2); //  2 millisecond pause - enable pulse must be > 450ns
+   digitalWrite (LCD_E, LOW) ; 
+}
+
+/*
+  send a byte to the lcd in two nibbles
+  before calling use SetChrMode or SetCmdMode to determine whether to send character or command
+*/
+void lcd_byte(char bits)
+{
+  digitalWrite (LCD_D4,(bits & 0x10)) ;  
+  digitalWrite (LCD_D5,(bits & 0x20)) ;  
+  digitalWrite (LCD_D6,(bits & 0x40)) ;  
+  digitalWrite (LCD_D7,(bits & 0x80)) ;  
+  pulseEnable();
+
+  digitalWrite (LCD_D4,(bits & 0x1)) ;  
+  digitalWrite (LCD_D5,(bits & 0x2)) ;  
+  digitalWrite (LCD_D6,(bits & 0x4)) ;  
+  digitalWrite (LCD_D7,(bits & 0x8)) ;  
+  pulseEnable();      	
+}
+
+void SetCmdMode() 
+{
+  digitalWrite (LCD_RS, 0); // set for commands
+}
+
+void SetChrMode() 
+{
+  digitalWrite (LCD_RS, 1); // set for characters
+}
+
+void lcd_text(char *s)
+{
+  while(*s) 
+	lcd_byte(*s++);
+ }
+
+void lcd_init()
+{
+   wiringPiSetupGpio () ; // use BCIM numbering
+   // set up pi pins for output
+   pinMode (LCD_E,  OUTPUT);
+   pinMode (LCD_RS, OUTPUT);
+   pinMode (LCD_D4, OUTPUT);
+   pinMode (LCD_D5, OUTPUT);
+   pinMode (LCD_D6, OUTPUT);
+   pinMode (LCD_D7, OUTPUT);
+   
+   //pinMode (GPIO_LED, OUTPUT); // added a led gpio for indication
+   // initialise LCD
+   SetCmdMode(); // set for commands
+   lcd_byte(0x33); // full init 
+   lcd_byte(0x32); // 4 bit mode
+   lcd_byte(0x28); // 2 line mode
+   lcd_byte(0x0C); // display on, cursor off, blink off
+   lcd_byte(0x01);  // clear screen
+   delay(3);        // clear screen is slow!
+}
+
+void lcd_clear()
+{
+   SetCmdMode();
+   lcd_byte(0x01);  // clear screen
+   delay(3);        // clear screen is slow!
+}
+
+
+void lcd_print(char *msg) 
+{
+  SetCmdMode();    // set for commands
+  delay(2);
+  lcd_byte(0x01);  //Clear screen
+  delay(3);
+  lcd_byte(0x02);
+  delay(3);
+  lcd_byte(0x80);  // set home loc
+  delay(3);
+  SetChrMode(); 
+  lcd_text(msg);
+  delay(5);
+}
+
+/*
+ 
+int main() 
+{
+  lcd_init();
+  
+  SetChrMode(); 
+  
+  char buffer_tanmay[20] = "Tanmay Kothale";
+  char buffer_varun[20] = "Varun Mehta";
+  char buffer_amey[20] = "Amey Dashaputre";
+  
+  //for testing purposes, print a hard coded
+  //string, clear it, print another, and repeat it
+  //forever
+  while (1)
+  {
+     lcd_clear();
+     delay(1000);
+     lcd_print(buffer_tanmay);
+     delay(5000);
+     	
+     lcd_clear();
+     delay(1000);
+     lcd_print(buffer_varun);
+     delay(5000);
+     
+     lcd_clear();
+     delay(1000);
+     lcd_print(buffer_amey);
+     delay(5000);
+  }
+  
+  return 0 ;
+}
+*/
--- sample_socket/socket_client/socket_client.c
+++ sample_socket/socket_client/socket_client.c
@@ -8,6 +8,9 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include "lcd.h"
+#include "wiringPi.h"
+
 #define SIZE 80
 #define PORT 8080
 #define SA struct sockaddr
@@ -21,14 +24,51 @@
 void func(int sockfd)
 {
     char buffer[SIZE];
-    while(1) {
-    	//clear the buffer
+    char buffer2[SIZE];
+    //int count = 0;
+    int flag = 0;
+    int j = 0; 
+    int i=0;
+    while(1) 
+    {
+        //clear the buffer
         bzero(buffer, sizeof(buffer));
+        bzero(buffer2, sizeof(buffer2));
+        
         //wait here till there is data to be read from server
-	//read client message and copy that in the buffer
-	while(read(sockfd, buffer, sizeof(buffer))==0); 
+        //read client message and copy that in the buffer
+        while(read(sockfd, buffer, sizeof(buffer))==0); 
+        
+        i=-1; j =0; flag = 0;
+        while (++i<20)
+        {   
+            if (buffer[i] != '-' && flag != 1)
+            {
+                buffer2[j++] = buffer[i];
+            }
+            else
+            {
+                
+                flag = 1;
+                while (j < 16)
+                {
+                    buffer[i] = ' ';
+                    buffer2[j++] = ' ';
+                }
+                buffer2[j++] = buffer[i];
+            }
+        }
+        
+        //display it on LCD
+        lcd_clear();
+        delay(1000);
+        lcd_print(buffer2);
+        
+        //printf("For LCD : %s\n\r", buffer2);
+        
         //print the message received from server
         printf("From Server : %s\n\r", buffer);
+
     }
 }
 
@@ -40,6 +80,10 @@ void func(int sockfd)
 ***********************************************************************************************/   
 int main()
 {
+    
+    lcd_init();
+    SetChrMode(); 
+    
     int sockfd;
     struct sockaddr_in servaddr;
    
@@ -56,7 +100,7 @@ int main()
    
     // assign IP, PORT
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("10.0.0.172");
+    servaddr.sin_addr.s_addr = inet_addr("10.0.0.85");
     servaddr.sin_port = htons(PORT);
    
     if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0)      // connect the client socket to server socket
@@ -67,7 +111,8 @@ int main()
     else
         printf("connected to the server..\n");
    
-    func(sockfd);     // function for chat
+    func(sockfd);  
    
     close(sockfd);    // close the socket
 }
+
